b78073f1e1342b1f49cd8f4da2df20cf47cb6c12
nschwalb1 <neilschwalb@gmail.com>
2017-09-20 18:16:03 -0400

    Added config file

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://test:1xhonz27eryh@ds121464.mlab.com:21464/assignment3_listings', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
